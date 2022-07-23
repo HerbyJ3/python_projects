c2adb185833974f65010ddc276b70b4b261537ec
Herby J <jeanty.herby@gmail.com>
2022-07-23 01:28:11 -0400

    adding Fizbuzz exercise from ACG

---
+++ FizzBuzzACG.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.7
+
+number = int(input("Enter an integer: "))
+if number % 5 == 0 and number % 3 == 0:
+    print("FizzBuzz")
+elif number % 3 == 0:
+    print("Fizz")
+elif number % 5 == 0:
+    print("Buzz")
+else:
+    print(number)
